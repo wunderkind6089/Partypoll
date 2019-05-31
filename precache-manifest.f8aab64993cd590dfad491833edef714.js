b8d6713f1ed06fd65a6edc3504fb219208502052
Abdulquadri <olayanju.olaide@olivetsolutions>
2019-05-31 17:25:08 +0100

    Updates

---
+++ precache-manifest.f8aab64993cd590dfad491833edef714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a584b780b012043363a20dcbb4b4ea",
+    "url": "/wunderkind6089/Partypoll/index.html"
+  },
+  {
+    "revision": "8fe0bf36b439b433ba92",
+    "url": "/wunderkind6089/Partypoll/static/css/2.74d8aad0.chunk.css"
+  },
+  {
+    "revision": "c9a1a914a0aaa37cf869",
+    "url": "/wunderkind6089/Partypoll/static/css/main.21dc5609.chunk.css"
+  },
+  {
+    "revision": "8fe0bf36b439b433ba92",
+    "url": "/wunderkind6089/Partypoll/static/js/2.1c2432c8.chunk.js"
+  },
+  {
+    "revision": "c9a1a914a0aaa37cf869",
+    "url": "/wunderkind6089/Partypoll/static/js/main.ade3d561.chunk.js"
+  },
+  {
+    "revision": "2a10eb136528883ca9fa",
+    "url": "/wunderkind6089/Partypoll/static/js/runtime~main.2371163b.js"
+  }
+]);
